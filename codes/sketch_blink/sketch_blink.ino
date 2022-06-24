5e6cdc139b1bb503e18fb954a966a4522b59b861
haldarsaurav <59361412+haldarsaurav@users.noreply.github.com>
2022-06-24 15:49:23 +0200

    added video

--- codes/sketch_blink/sketch_blink.ino
+++ codes/sketch_blink/sketch_blink.ino
@@ -1,27 +1,12 @@
-//***************************************************************************************
-//  Description; Output an alternating high and low voltage to P1.0,
-//     turning a red LED on the MSP430G2 Launch Pad on and off
-//***************************************************************************************
-
-void setup()
-{
-  // start the serial monitor
-  Serial.begin(9600);
-  
-  // set P1.0 to output
-  pinMode(P1_0, OUTPUT);
-  Serial.println("Pin 1.0 set as output");
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-  digitalWrite(P1_0, HIGH);  // set the pin to high, turning LED on
-  Serial.println("LED on");
-  
-  sleep(1000);               // wait 1000 ms
-  
-  digitalWrite(P1_0, LOW);   // set the pin to low, turning LED off
-  Serial.println("LED off");
-  
-  sleep(1000);               // wait 1000 ms
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
 }
